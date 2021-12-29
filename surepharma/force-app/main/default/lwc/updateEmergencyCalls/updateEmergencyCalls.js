62f55cd13f6fe91c5dc43b92dc7422205498d96f
Blake Holleman <blake.holleman@gmail.com>
2021-12-28 19:29:28 -0500

    add server-server GoogleConnect functionality and update permissions

--- surepharma/force-app/main/default/lwc/updateEmergencyCalls/updateEmergencyCalls.js
+++ surepharma/force-app/main/default/lwc/updateEmergencyCalls/updateEmergencyCalls.js
@@ -1,6 +1,6 @@
 import { LightningElement, api} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import clientHandler from '@salesforce/apex/EmergencyCallLogCallout.clientHandler';
+import clientHandler from '@salesforce/apex/GoogleConnect.clientHandler'; //'@salesforce/apex/EmergencyCallLogCallout.clientHandler';
 
 export default class UpdateEmergencyCalls extends LightningElement {
     isExecuting = false;
@@ -29,34 +29,7 @@ export default class UpdateEmergencyCalls extends LightningElement {
                     })
                 )
             })
-        /*    
-        await this.updateCalls()
-            .then( (success) => {this.dispatchEvent(success);})
-            .catch( (e) => {this.dispatchEvent(e);})
-        */
+
         this.isExecuting = false;
     }
-    
-    /*
-    updateCalls() {
-        var success = new ShowToastEvent({
-            title: 'Success!',
-            message: 'Emergency Call records have been updated'
-        });
-
-        var failure = new ShowToastEvent({
-            title: 'Error',
-            message: 'Callout to Emergency Call service failed'
-        })
-        
-        return new Promise( (resolve, reject) => {
-            var value = clientHandler();
-            if (value == 'Success') {
-                resolve(success);
-            } else {
-                reject(failure);
-            }
-            });
-    }
-    */
 }
